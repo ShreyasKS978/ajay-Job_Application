f3f77304f94dc96651499be93be4f5d1979e60da
Shreyas <shreyasks978@gmail.com>
2025-07-04 13:44:08 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const rateLimit = require('express-rate-limit');
 
 // Initialize Express app
 const app = express();
-const port = process.env.PORT || 3221;
+const port = process.env.PORT || 3049;
 
 // Create uploads directory if it doesn't exist
 const uploadsDir = path.join(__dirname, 'Uploads');
@@ -36,13 +36,13 @@ app.use(morgan('dev'));
 
 // CORS configuration
 const allowedOrigins = [
-  'http://54.166.206.245:7771',
-  'http://54.166.206.245:7772',
-  'http://54.166.206.245:7773',
-  'http://54.166.206.245:3221',
-  'http://localhost:7771',
-  'http://localhost:7772',
-  'http://localhost:7773',
+  'http://13.232.40.109:8158',
+  'http://13.232.40.109:8159',
+  'http://13.232.40.109:8160',
+  'http://13.232.40.109:3049',
+  'http://localhost:8158',
+  'http://localhost:8159',
+  'http://localhost:8160',
   'http://127.0.0.1:5500'
 ];
 
